601d4865601d2ecd63350ef4c20d56a247a102ae
saksham08jain <92016981+saksham08jain@users.noreply.github.com>
2022-04-06 16:04:55 +0530

    Add files via upload
    added files

---
+++ patt.js
@@ -0,0 +1,4 @@
+// Created by Saksham jain only turtle Actually a python code
+
+alert("Kindly take a screenshot of your pattern if you find it beautiful and share it here (all things are random) See my comments on this code for more beautiful patterns😉😀")
+alert("click on screen when drawing completes to get more drawings")
